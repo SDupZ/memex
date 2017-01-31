ed9e7b0978e46ebbfdab4c6619a0c123bad8bdc7
Simon du Preez <simon.dupreez@icg.co.nz>
2017-01-31 17:45:43 +1300

    Updated memepagechart

--- static/js/src/handleMemepageCharts.js
+++ static/js/src/handleMemepageCharts.js
@@ -1,10 +1,46 @@
+function generateDaysAndData() {
+    for(var i=0;i<40;i++){
+      days[i] = i+1;
+      data[i] = number;
+      var rand = getRandomInt(0,3);
+      if(rand == 1) {
+          number = number + 1;
+      } else if(rand == 2) {
+          number = number - 1;
+      }
+    }
+}
+
+
+function getScaleHeight(data) {
+    var total = 0;
+    for(var i = 0; i < data.length; i++) {
+        total += data[i];
+    }
+    var avg = total / data.length;
+    var scaleHeight = 0;
+    if (Math.max(data) > 2 * avg) {
+        scaleHeight = Math.max(data);
+    } else {
+        scaleHeight = 2 * avg;
+    }
+    return Math.ceil(scaleHeight);
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 var days = [];
 var data = [];
+var number = 20;
 
-for(var i=0;i<40;i++){
-  days[i] = i;
-  data[i] = getRandomInt(0,50);
-}
+generateDaysAndData();
+var scaleHeight = getScaleHeight(data);
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
@@ -15,23 +51,38 @@ var myChart = new Chart(ctx, {
             label: 'Popularity of X',
             data: data,
             backgroundColor: 'rgba(63,81,181,0.2)',
-            borderColor: ['rgba(63,81,181,1)',],
-            borderWidth: 2
+            borderColor: 'rgba(63,81,181,1)',
+
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(63,81,181,1)",
+            pointBackgroundColor: "rgba(63,81,181,0.2)",
+            pointHoverRadius: 7,
+            pointHoverBackgroundColor: "rgba(63,81,181,0.8)",
+            pointRadius: 3,
+            borderWidth: 3,
+            pointHitRadius: 20,
         }]
     },
     options: {
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
         scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false
+                }
+            }],
             yAxes: [{
+                gridLines: {
+                    display: false
+                },
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero: true,
+                    max: scaleHeight,
                 }
             }]
         }
     }
 });
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
