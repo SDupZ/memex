dc4cc0454da1f97029c5405db9cdbe8a7adfff90
Simon du Preez <simon.dupreez@icg.co.nz>
2017-02-02 16:36:11 +1300

    Updated scripts and added indico

--- memexscripts/botscripts/redditbot.py
+++ memexscripts/botscripts/redditbot.py
@@ -4,10 +4,26 @@ import re
 
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("all")
 
-for submission in subreddit.hot(limit=30):
-        if re.search("trump", submission.title, re.IGNORECASE) or re.search("trump", submission.selftext, re.IGNORECASE):
-            print(submission.title)
-            print(submission.selftext)
-            print("=========================")
+subreddit = reddit.subreddit("memeeconomy")
+keyword = 'pepe'
+
+print("===========BEGIN==============")
+
+# for submission in subreddit.hot(limit=100):
+for submission in subreddit.top(time_filter='all'):
+    print("Searching title and text for: ")
+    print(submission.title)
+    if re.search(keyword, submission.title, re.IGNORECASE) or re.search(keyword, submission.selftext, re.IGNORECASE):
+        print("MATCH FOUND: " + submission.title.encode('utf-8'))
+        print(submission.selftext.encode('utf-8'))
+
+    print("Searching comments...")
+    for comment in submission.comments:
+        try:
+            if re.search(keyword, comment.body, re.IGNORECASE) or re.search(keyword, comment.body, re.IGNORECASE):
+                print("MATCH FOUND: " + comment.body.encode('utf-8'))
+        except:
+            pass
+
+    print("=========================")
