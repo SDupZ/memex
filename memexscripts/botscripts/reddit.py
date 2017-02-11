b20e59de8372a4de38258397b8df61e8016299ee
Simon du Preez <smdupnz@yahoo.co.nz>
2017-02-12 00:21:57 +1300

    Changed reddit.py script to dump data from adviceadminals to results.json

--- memexscripts/botscripts/reddit.py
+++ memexscripts/botscripts/reddit.py
@@ -54,18 +54,29 @@ subreddit = reddit.subreddit("adviceanimals")
 
 print("===========BEGIN==============")
 
-reddit_data = {}
+reddit_data = []
 
-for submission in subreddit.hot(limit=50):
+for submission in subreddit.hot(limit=10):
     if not (submission.stickied):
         print submission.title
-        # try:
-        meme_url = parse_reddit_url(submission.url)
-        result = generate_probabilities(meme_url)
-        highest_similarity = max(result, key=lambda i: result[i])
-        reddit_data[submission.id] = {"meme": highest_similarity, "similarity":result[highest_similarity] }
-        # except Exception as e:
-        #     print "ERRUH " + str(e) + " : " + str(submission.title)
+        try:
+            meme_url = parse_reddit_url(submission.url)
+            result = generate_probabilities(meme_url)
+            highest_similarity = max(result, key=lambda i: result[i])
+            reddit_data.append(
+            {   'id': submission.id,
+                'meme': highest_similarity,
+                'similarity':result[highest_similarity],
+                'url': submission.url,
+                'gilded': submission.gilded,
+                'score': submission.score,
+                'title': submission.title,
+                'subreddit': str(submission.subreddit),
+                'subreddit_id': submission.subreddit_id,
+                'permalink': submission.permalink
+            })
+        except Exception as e:
+            print "ERRUH " + str(e) + " : " + str(submission.title)
 
-json.dump(reddit_data, open('results.json', 'wb'))
+json.dump(reddit_data, open('results.json', 'wb'), indent=4, sort_keys=True)
 print("===========DONE==============")
