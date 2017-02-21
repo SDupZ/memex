b85bd55b1203f3dd197ae26ca5042d7c3f0ba2ee
sdupz <dupreez.s.m@gmail.com>
2017-02-22 00:00:50 +1300

    Added csrf decorator for githook

--- default/views.py
+++ default/views.py
@@ -1,6 +1,9 @@
+
 from django.shortcuts import render
 from blog.models import BlogPost
 from django.http import HttpResponse, Http404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 import git
 
@@ -10,11 +13,12 @@ def home(request):
     return render(request, template, context)
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class Githook(View):
     def get(self, request):
         raise Http404
 
-    def post(self, request, *args, **kwargs)
-        g = git.cmd.Git(git_dir)
+    def post(self, request, *args, **kwargs):
+	g = git.cmd.Git(git_dir)
         g.pull()
         return HttpResponse("OK")
