4184fefe39cc7346db82cebfa438acf7d57f1fa7
Simon du Preez <smdupnz@yahoo.co.nz>
2017-03-26 22:51:19 +1300

    Added admin fieldsets to data. Added created dasta to listview

--- data/admin.py
+++ data/admin.py
@@ -3,8 +3,19 @@ from data.models import MemeDataSnapshot24Hour
 
 
 class MemeDataSnapshot24HourAdmin(admin.ModelAdmin):
-    list_display = ('meme', 'crs_l24hrs', 'np_l24hrs',)
+    list_display = ('meme', 'crs_l24hrs', 'np_l24hrs', 'created_date')
     search_fields = ('meme',)
     ordering = ('meme',)
+
+    fieldsets = (
+        ('Snapshot', {
+            'fields': ('meme', 'crs_l24hrs', 'np_l24hrs',),
+        }),
+        ('Admin', {
+            'classes': ('collapse',),
+            'fields': ('created_date', 'modified_date')
+        })
+    )
+    readonly_fields = ('created_date', 'modified_date')
 
 admin.site.register(MemeDataSnapshot24Hour, MemeDataSnapshot24HourAdmin)
